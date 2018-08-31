05d24530773bd23cbc8de1b3dc5c50b96a783368
Mika Morgan <mmorgan@archercityisd.net>
2018-08-31 09:02:42 -0500

    Add main room

--- AGOD/js/mainRoom.js
+++ AGOD/js/mainRoom.js
@@ -2,10 +2,15 @@ var mainRoom = {
     
 preload: function() {
     console.log('In Main Room');
+    game.load.image('mainRoom','assets/mainRoom.jpg');
 },
 
 create: function() {
+    //Create background
+    this.bg = game.add.sprite(0,0,'mainRoom');
+    this.bg.scale.setTo(2.75,2.7);
 
+    //Set up for keyboard input
     k = game.input.keyboard;
 },
 
