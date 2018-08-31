2f38bb8963be1708c3355da3d9c508b705c113b1
Mika Morgan <mmorgan@archercityisd.net>
2018-08-31 14:40:17 -0500

    Add new portals

--- AGOD/js/mainRoom.js
+++ AGOD/js/mainRoom.js
@@ -3,12 +3,34 @@ var mainRoom = {
 preload: function() {
     console.log('In Main Room');
     game.load.image('mainRoom','assets/mainRoom.jpg');
+    game.load.spritesheet('portal', 'assets/portals.jpg', 80, 80);
 },
 
 create: function() {
     //Create background
     this.bg = game.add.sprite(0,0,'mainRoom');
     this.bg.scale.setTo(2.75,2.7);
+
+    //Create first portal
+    this.portal1 = game.add.sprite(400, 380, 'portal');
+    this.portal1.animations.add('spin', [0, 1, 2], 30, true);
+    this.portal1.animations.play('spin');
+
+    //Create second portal
+    this.portal2 = game.add.sprite(400, 550, 'portal');
+    this.portal2.animations.add('spin', [3, 4, 5], 30, true);
+    this.portal2.animations.play('spin');
+
+     //Create third portal
+     this.portal3 = game.add.sprite(1050, 380, 'portal');
+     this.portal3.animations.add('spin', [6, 7, 8], 30, true);
+     this.portal3.animations.play('spin');
+ 
+     //Create fourth portal
+     this.portal4 = game.add.sprite(1050, 550, 'portal');
+     this.portal4.animations.add('spin', [9, 10, 11], 30, true);
+     this.portal4.animations.play('spin');
+
 
     //Set up for keyboard input
     k = game.input.keyboard;
