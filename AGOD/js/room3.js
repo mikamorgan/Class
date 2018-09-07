fe3c8187b3eac832e174b6d21a2c6443fc13da03
Mika Morgan <mmorgan@archercityisd.net>
2018-09-07 10:27:17 -0500

    add trinket spritesheet and plant animation

--- AGOD/js/room3.js
+++ AGOD/js/room3.js
@@ -2,11 +2,18 @@ var room3 = {
     preload: function() {
         console.log('In Room 3');
         game.load.image('room3','assets/room3.png');
+        game.load.spritesheet('trinkets','assets/trinkets.png', 32, 32);
     },
     
     create: function() {
         this.bg = game.add.sprite(0,0,'room3');
         this.bg.scale.setTo(2.75,2.7);
+
+        this.plant = game.add.sprite(150, 300, 'trinkets');
+        this.plant.animations.add('grow', [28,29,30,31,40,41,42,43],10,true);
+        this.plant.play('grow');
+
+
     },
     
     update: function() {
