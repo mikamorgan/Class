d8292fd6236a74f405c4ccfdab14dc757fff9afd
Mika Morgan <mmorgan@archercityisd.net>
2018-09-28 11:02:36 -0500

    Finish sounds

--- AGOD/js/gameOver.js
+++ AGOD/js/gameOver.js
@@ -2,12 +2,18 @@ var gameOver = {
     preload: function() {
         console.log('In Game Over');
         game.load.image('BG','assets/GO.png');
+
+        game.load.audio('loseSound', 'assets/loseSound.wav');
     },
     
     create: function() {
         //Create background
         this.bg = game.add.sprite(0,0,'BG');
         this.bg.scale.setTo(2);
+
+        this.loseSound = game.add.audio('loseSound');
+        this.loseSound.volume = .1;
+        this.loseSound.play();
     },
     
     update: function() {
